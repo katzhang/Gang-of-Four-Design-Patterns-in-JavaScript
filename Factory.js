2c44576a8e69e285d9bb0cb283bae1c192eb54ff
katzhang <arcadychang@gmail.com>
2014-10-23 20:40:39 -0400

    added builder

--- Factory.js
+++ Factory.js
@@ -21,3 +21,86 @@ var mazeGame = (function()
 		}
 	}
 }());
+
+/*
+Essential JS Design Patterns: http://addyosmani.com/resources/essentialjsdesignpatterns/book/#factorypatternjavascript
+example of how a Vehicle Factory is implemented
+*/
+function Car (options) {};
+
+function Truck(options) {};
+
+//Define a skeleton vehicle factory
+function VehicleFactory() {};
+
+//By default it is Car
+VehicleFactory.prototype.vehicleClass = Car;
+
+VehicleFactory.prototype.createVehicle = function (options) {
+	switch(options.vehicleType) {
+		case "car":
+			this.vehicleClass = Car;
+			break;
+		case "truck":
+			this.vehicleClass = Truck;
+			break;
+	}
+
+	return new this.vehicleClass(options);
+
+}
+
+//Usage
+var carFactory = new VehicleFactory();
+var car = carFactory.createVehicle({
+	vehicleType: 'car'
+})
+
+//Scenario 1: modify a VehicleFactory instance to use the Truck class
+var truck = carFactory.createVehicle({
+	vehicleType: 'truck'
+})
+
+//Scenario 2: subclass VehicleFactory class to create a factory that builds trucks
+function TruckFactory() {};
+
+TruckFactory.prototype = new VehicleFactory();
+TruckFactory.prototype.vehicleClass = Truck;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
