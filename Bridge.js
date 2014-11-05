d105a0c40d61a3bad072ff97226348c8cd0bebe6
katzhang <arcadychang@gmail.com>
2014-11-04 22:55:14 -0500

    added Bridge

---
+++ Bridge.js
@@ -0,0 +1,5 @@
+/* 
+Bridge pattern allows a client and a service to work together with each of them having its own interface --> two levels of abstraction
+http://www.dofactory.com/javascript/bridge-design-pattern
+*/
+
